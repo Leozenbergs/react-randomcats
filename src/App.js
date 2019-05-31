5dcab4fd103fd4c74683a7af02bc759123edc671
Leonardo Rodrigues <leoabreu14@gmail.com>
2019-05-30 23:57:11 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.getCats = this.getCats.bind(this);
+    this.state = {
+      image: ""
+    }
+  }
+
+  getCats = ()=>{
+    // console.log("teste");
+    axios.get('https://api.thecatapi.com/v1/images/search', {
+      headers: {                  
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Authorization", 
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
+        "Content-Type": "application/json;charset=UTF-8"                   
+      }
+    })
+    .then(res => {
+      // console.log(res.data[0].url);
+      let src = res.data[0].url;
+      this.setState({
+        image: res.data[0].url
+      });
+      // console.log(this.state.main_image);
+    })
+      .catch(error =>
+        console.log(error));
+  }
+  componentDidMount(){
+    this.getCats();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h2>Hi!</h2>
+        <img src={this.state.image} max-width="100%"/>
+      </div>
+    );
+  }
 }
 
 export default App;
