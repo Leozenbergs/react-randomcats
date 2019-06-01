94443f9fad46845a3a33af199d92443e2d546033
Leonardo Rodrigues <leoabreu14@gmail.com>
2019-05-31 21:41:29 -0300

    Updates

---
+++ precache-manifest.1db4b984f643e80b2be40ce66dfa9603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac258b867e41f01cbfecaf47cee2f9d",
+    "url": "/react-randomcats/index.html"
+  },
+  {
+    "revision": "33c6a2031bac64537e33",
+    "url": "/react-randomcats/static/css/main.1839af0c.chunk.css"
+  },
+  {
+    "revision": "04a12cf2851af2915c91",
+    "url": "/react-randomcats/static/js/2.1a43b971.chunk.js"
+  },
+  {
+    "revision": "33c6a2031bac64537e33",
+    "url": "/react-randomcats/static/js/main.5b282c73.chunk.js"
+  },
+  {
+    "revision": "78bfad59b7a77001ad51",
+    "url": "/react-randomcats/static/js/runtime~main.e92b0c06.js"
+  }
+]);
